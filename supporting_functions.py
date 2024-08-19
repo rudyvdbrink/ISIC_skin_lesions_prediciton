e515a2c6559c632c9ce8bd1d5310ace6501bbe48
Rudy <30622165+rudyvdbrink@users.noreply.github.com>
2024-08-19 07:32:40 +0200

    Initial pipeline setup

--- supporting_functions.py
+++ supporting_functions.py
@@ -2,7 +2,7 @@
 import matplotlib.pyplot as plt
 
 # %%
-def plot_images_grid(images_array, start_N=0, num_images=12, grid_shape=(3, 4)):
+def plot_images_grid(images_array, metadata, start_N=0, num_images=12, grid_shape=(3, 4)):
     # Create a figure with a specified size
     fig, axes = plt.subplots(grid_shape[0], grid_shape[1], figsize=(15, 10))
     
@@ -14,6 +14,13 @@ def plot_images_grid(images_array, start_N=0, num_images=12, grid_shape=(3, 4)):
             # Plot each image
             axes[i].imshow(images_array[i+start_N])
             axes[i].axis('off')  # Hide the axis
+
+            # Get the file name (isic_id) and diagnosis from metadata
+            isic_id = metadata.loc[i, 'isic_id']
+            diagnosis = metadata.loc[i, 'diagnosis']
+
+            # Set the title with file name and diagnosis
+            axes[i].set_title(f"{isic_id}\nDiagnosis: {diagnosis}", fontsize=10)
 
     # Remove any unused subplots
     for j in range(num_images, len(axes)):
