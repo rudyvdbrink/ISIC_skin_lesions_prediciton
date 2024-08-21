b883d02d85238ecb5192d70504fd763c91d3de07
Rudy van den Brink <30622165+rudyvdbrink@users.noreply.github.com>
2024-08-21 09:00:25 +0200

    added mnist control

---
+++ control/1_clean_control_data.py
@@ -0,0 +1,73 @@
+# %% import libraries
+
+import numpy as np
+import pandas as pd
+import pickle
+
+from sklearn.model_selection import train_test_split
+
+from supporting_functions import plot_images_grid_nometa
+
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.utils import resample
+
+from tensorflow.keras.datasets import mnist
+
+# %% load mnist data
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data() 
+
+
+# %% rebalance data (super-sample less frequent class)
+
+# Flatten the input data temporarily to use with RandomOverSampler
+X_train_flat = X_train.reshape((X_train.shape[0], -1))  # Reshape to (9376, 90000)
+
+
+# Apply RandomOverSampler to balance the classes
+ros = RandomOverSampler(sampling_strategy='auto', random_state=42)
+X_train_resampled, y_train_resampled = ros.fit_resample(X_train_flat, y_train)
+
+# Reshape X_train back to the original shape
+X_train_resampled = X_train_resampled.reshape((-1, X_train.shape[1], X_train.shape[2]))
+
+# Verify the class distribution after resampling
+unique, counts = np.unique(y_train_resampled, return_counts=True)
+print(f'Class distribution after resampling: {dict(zip(unique, counts))}')
+
+# %% sub-sample data to its original size
+
+# Sub-sample the resampled data to match the original size
+X_train_subsampled, y_train_subsampled = resample(
+    X_train_resampled, y_train_resampled, 
+    replace=False,  # Do not replace, we want exactly original size
+    n_samples=X_train.shape[0],  # Original size
+    random_state=42  # For reproducibility
+)
+
+# Verify the shape
+print(f"Sub-sampled X_train shape: {X_train_subsampled.shape}")
+print(f"Sub-sampled y_train shape: {y_train_subsampled.shape}")
+
+
+# %% over-write original variables and save
+
+X_train = X_train_subsampled
+y_train = y_train_subsampled
+
+# %% save data
+
+data_to_save = [X_train, X_test, y_train, y_test]
+
+print('Saving data...')
+# save data to file so we don't have to run it again
+with open('data/processed/mnist_data.pkl','wb') as f:
+    pickle.dump(data_to_save,f) 
+print('All done!')
+
+#plot_images_grid(data,metadata_full,start_N=0)
+
+#%%
+
+plot_images_grid_nometa(X_train,start_N=0)
+
---
+++ control/2_control_try_simple_cnn.py
@@ -0,0 +1,117 @@
+# %% get libraries
+import numpy as np
+import pickle
+import time
+
+from sklearn import metrics
+import tensorflow as tf
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.utils import to_categorical
+from sklearn.utils.class_weight import compute_class_weight
+from tensorflow.keras.applications import VGG16
+
+import visualkeras
+
+# %% load data
+
+with open('data/processed/mnist_data.pkl', 'rb') as f:
+    loaded_data = pickle.load(f)
+
+X_train            = loaded_data[0]
+X_test             = loaded_data[1]
+y_train            = loaded_data[2]
+y_test             = loaded_data[3]
+
+# %% add extra dimension 
+
+X_train = np.expand_dims(X_train,axis=3)
+X_test = np.expand_dims(X_test,axis=3)
+
+
+#%% reshape input data
+
+#reshape X_train to the appropriate 4D shape (9376, 450, 600, 3)
+# X_train = X_train.reshape((9376, 450, 600, 3))
+# X_test  = X_test.reshape((2344, 450, 600, 3))
+
+# %% sub-sample
+
+# X_train = X_train[:,::3,::3,:]
+# X_test  = X_test[:,::3,::3,:]
+
+
+# %%
+
+#one-hot encode the labels
+num_classes = len(np.unique(y_train))
+y_train_encoded = to_categorical(y_train, num_classes)
+
+
+# %% set up model
+
+model = tf.keras.Sequential([
+    tf.keras.Input(shape=(np.shape(X_train)[1],np.shape(X_train)[2],np.shape(X_train)[3])),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.Flatten(), #turn output of previous layer into a vector rather than tensor
+    tf.keras.layers.Dense(units=20,activation='relu'),
+    tf.keras.layers.Dense(units=num_classes, activation='softmax')
+])
+
+# %% info on model
+
+
+model.summary()
+
+#font = ImageFont.truetype("arial.ttf", 32) 
+visualkeras.layered_view(model,legend=True, scale_xy=1, scale_z=0.01)
+
+
+# %% image augmentation (we over-sampled )
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# Create an ImageDataGenerator with random rotations and other augmentations
+datagen = ImageDataGenerator(
+    rotation_range=30,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+#fit the generator on data
+datagen.fit(X_train)
+
+# %% compile and fit
+
+#model.compile(optimizer='sgd',loss='categorical_crossentropy',metrics=['accuracy'])
+model.compile(optimizer='Adam',loss='categorical_crossentropy')
+
+# Train the model using the generator
+model.fit(datagen.flow(X_train, to_categorical(y_train, num_classes=num_classes), batch_size=32),
+          steps_per_epoch=len(X_train) // 32,
+          epochs=20,
+          )
+model.summary()
+
+# %%
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+pred = model.predict(X_test)
+pred = np.argmax(pred,axis=1)
+print('Accuracy = ' + str(np.mean(pred==y_test)))
+print('Balanced accuracy = ' + str(metrics.balanced_accuracy_score(y_test,pred)))
+
+#plt.imshow(metrics.confusion_matrix(y_test,pred))
+sns.heatmap(metrics.confusion_matrix(y_test,pred) / len(pred),linecolor='white',linewidths=0.05)
+plt.xlabel("true label")
+plt.ylabel("predicted label")
+plt.title('Balanced accuracy = ' + str(metrics.balanced_accuracy_score(y_test,pred)))
+plt.show()
+
