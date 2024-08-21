4afca28bff39ad4eda1f29872b11b26a42e59510
Rudy van den Brink <30622165+rudyvdbrink@users.noreply.github.com>
2024-08-21 08:59:39 +0200

    Update 2a_train_model.py

--- 2a_train_model.py
+++ 2a_train_model.py
@@ -3,11 +3,15 @@ import numpy as np
 import pickle
 import time
 
+from sklearn import metrics
+import tensorflow as tf
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from tensorflow.keras.utils import to_categorical
 from sklearn.utils.class_weight import compute_class_weight
 from tensorflow.keras.applications import VGG16
+
+import visualkeras
 
 # %% load data
 
@@ -21,100 +25,89 @@ y_test             = loaded_data[3]
 labels             = loaded_data[4]
 metadata           = loaded_data[5]
 
-# %% set up model
+#%% reshape input data
 
-# 1. Reshape X_train to the appropriate 4D shape (9376, 450, 600, 3)
-X_train = X_train.reshape((9376, 450, 600, 3))
+#reshape X_train to the appropriate 4D shape (9376, 450, 600, 3)
+# X_train = X_train.reshape((9376, 450, 600, 3))
+# X_test  = X_test.reshape((2344, 450, 600, 3))
 
-# 2. One-hot encode the labels
-num_classes = len(np.unique(y_train))
-y_train_encoded = to_categorical(y_train, num_classes)
+# %% sub-sample
 
-# Load the VGG16 model with pre-trained ImageNet weights, without the top layers
-base_model = VGG16(weights='imagenet', include_top=False, input_shape=(450, 600, 3))
+X_train = X_train[:,::3,::3,:]
+X_test  = X_test[:,::3,::3,:]
 
-# Freeze the base model layers to prevent them from being updated during training
-base_model.trainable = False
-
-# Add custom layers on top of the base model
-x = base_model.output
-x = Flatten()(x)
-x = Dense(256, activation='relu')(x)
-x = Dropout(0.5)(x)
-x = Dense(128, activation='relu')(x)
-x = Dropout(0.5)(x)
-output_layer = Dense(num_classes, activation='softmax')(x)
-
-# Combine base model with custom layers
-model = Model(inputs=base_model.input, outputs=output_layer)
-
-# Compile the model
-model.compile(optimizer='adam', 
-              loss='categorical_crossentropy', 
-              metrics=['accuracy'])
-
-# 5. Compute class weights to handle the class imbalance
-class_weights = compute_class_weight(class_weight='balanced', classes=np.unique(y_train), y=y_train)
-class_weights_dict = dict(enumerate(class_weights))
-
-# %% fit
-
-# Start the timer
-start_time = time.time()
-
-# Fit the model
-model.fit(X_train, y_train_encoded, 
-          epochs=20, 
-          batch_size=32, 
-          class_weight=class_weights_dict,
-          validation_split=0.2)
-
-# End the timer
-end_time = time.time()
-
-# Calculate the elapsed time
-elapsed_time = end_time - start_time
-
-print('Fitting took ' + str(elapsed_time / 60) + ' minutes')
-
-# Print the model summary
-model.summary()
-
-# %% save the model
-
-# save model to file so we don't have to run it again
-with open('models/CNN_classifier.pkl','wb') as f:
-    pickle.dump(model,f)    
 
 # %%
 
-# # 3. Build custom CNN model
-# model = Sequential()
+#one-hot encode the labels
+num_classes = len(np.unique(y_train))
+y_train_encoded = to_categorical(y_train, num_classes)
 
-# # Convolutional layers
-# model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(450, 600, 3)))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
 
-# model.add(Conv2D(64, (3, 3), activation='relu'))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
+# %% set up model
 
-# model.add(Conv2D(128, (3, 3), activation='relu'))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
+model = tf.keras.Sequential([
+    tf.keras.Input(shape=(np.shape(X_train)[1],np.shape(X_train)[2],np.shape(X_train)[3])),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.Flatten(), #turn output of previous layer into a vector rather than tensor
+    tf.keras.layers.Dense(units=20,activation='relu'),
+    tf.keras.layers.Dense(units=num_classes, activation='softmax')
+])
 
-# # Flatten the output of the convolutional layers
-# model.add(Flatten())
+# %% info on model
 
-# # Fully connected layers
-# model.add(Dense(256, activation='relu'))
-# model.add(Dropout(0.5))
 
-# model.add(Dense(128, activation='relu'))
-# model.add(Dropout(0.5))
+model.summary()
 
-# # Output layer
-# model.add(Dense(num_classes, activation='softmax'))
+#font = ImageFont.truetype("arial.ttf", 32) 
+visualkeras.layered_view(model,legend=True, scale_xy=1, scale_z=0.01)
 
-# # 4. Compile the model
-# model.compile(optimizer='adam', 
-#               loss='categorical_crossentropy', 
-#               metrics=['accuracy'])
+
+# %% image augmentation (we over-sampled )
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# Create an ImageDataGenerator with random rotations and other augmentations
+datagen = ImageDataGenerator(
+    rotation_range=30,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+#fit the generator on data
+datagen.fit(X_train)
+
+# %% compile and fit
+
+#model.compile(optimizer='sgd',loss='categorical_crossentropy',metrics=['accuracy'])
+model.compile(optimizer='Adam',loss='categorical_crossentropy')
+
+# Train the model using the generator
+model.fit(datagen.flow(X_train, to_categorical(y_train, num_classes=num_classes), batch_size=32),
+          steps_per_epoch=len(X_train) // 32,
+          epochs=20,
+          )
+model.summary()
+
+# %%
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+pred = model.predict(X_test)
+pred = np.argmax(pred,axis=1)
+print('Accuracy = ' + str(np.mean(pred==y_test)))
+print('Balanced accuracy = ' + str(metrics.balanced_accuracy_score(y_test,pred)))
+
+#plt.imshow(metrics.confusion_matrix(y_test,pred))
+sns.heatmap(metrics.confusion_matrix(y_test,pred) / len(pred),linecolor='white',linewidths=0.05)
+plt.xlabel("true label")
+plt.ylabel("predicted label")
+plt.title('Balanced accuracy = ' + str(metrics.balanced_accuracy_score(y_test,pred)))
+plt.show()
+
